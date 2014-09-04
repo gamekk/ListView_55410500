e68551f79dfbbab480048f4e6a67ccc4b6e6e5e2
gamekk <gamebuu001@gmail.com>
2014-09-04 20:24:02 +0700

    ListView

---
+++ src/com/example/listview_500/List_items.java
@@ -0,0 +1,23 @@
+package com.example.listview_500;
+
+import android.graphics.drawable.Drawable;
+
+public class List_items {
+	private Drawable drawable;
+	private String title;
+	public Drawable getDrawable() {
+		return drawable;
+	}
+	public void setDrawable(Drawable drawable) {
+		this.drawable = drawable;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	
+		
+	}
